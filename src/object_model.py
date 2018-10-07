225292ca193674654e5267406a8ef85096942e57
Artem Smirnov <urpylka@gmail.com>
2018-10-07 08:22:38 +0300

    Final obj model

--- src/object_model.py
+++ src/object_model.py
@@ -9,10 +9,9 @@ import threading
 class AbstractState(object):
     stop_state = False
 
-    def __init__(self):
+    def __init__(self, _sm):
         print "Processing current state: " + str(self)
 
-    def do(self, _sm):
         t = threading.Thread(target=self.run, args=(_sm,))
         t.daemon = True
         t.start()
@@ -37,31 +36,29 @@ class AbstractState(object):
 
 class INIT(AbstractState):
     def run(self, _sm):
-        _sm.new_state(IDLE())
+        _sm.new_state(IDLE(_sm))
     def exec_command(self, command):
         print "Не могу выполнить команду: " + command
 
 class IDLE(AbstractState):
     def run(self, _sm):
-        print "IIIIIIDDDLLLEEEE"
         while True:
             if not self.stop_state:
                 time.sleep(1)
             else:
-                _sm.new_state(IDLE())
+                _sm.new_state(DELIVERY(_sm))
+                break
     def exec_command(self, command):
         print command
         self.stop_state = True
 
-class GOTO(AbstractState):
+class DELIVERY(AbstractState):
     def run(self, _sm):
-        print "GOTOTOT"
-        self.stop_state = True
         while True:
             if not self.stop_state:
                 time.sleep(1)
             else:
-                _sm.new_state(IDLE())
+                _sm.new_state(IDLE(_sm))
                 break
     def exec_command(self, command):
         print command
@@ -69,8 +66,7 @@ class GOTO(AbstractState):
 
 class StateMachine(object):
     def __init__(self):
-        self.state = INIT()
-        self.state.do(self)
+        self.state = INIT(self)
 
     def new_state(self, _state):
         self.state = _state
@@ -79,7 +75,17 @@ class StateMachine(object):
         self.state.exec_command(command)
 
 st = StateMachine()
-time.sleep(1)
-st.new_command("wefaf")
+
+
+
 time.sleep(3)
 st.new_command("412423")
+time.sleep(3)
+st.new_command("412423")
+time.sleep(3)
+st.new_command("412423")
+time.sleep(3)
+st.new_command("412423")
+time.sleep(3)
+st.new_command("412423")
+time.sleep(3)
