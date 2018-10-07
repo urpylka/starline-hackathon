150a3b4adddd3165abaf1deb4c2f2aaa8c655823
Artem Smirnov <urpylka@gmail.com>
2018-10-07 08:41:12 +0300

    New tbot

--- src/object_model.py
+++ src/object_model.py
@@ -74,7 +74,114 @@ class StateMachine(object):
     def new_command(self, command):
         self.state.exec_command(command)
 
-st = StateMachine()
+class GoToPose():
+    def __init__(self):
+        self.goal_sent = False
+
+        # What to do if shut down (e.g. Ctrl-C or failure)
+        rospy.on_shutdown(self.shutdown)
+
+        # Tell the action client that we want to spin a thread by default
+        self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
+        rospy.loginfo("Wait for the action server to come up")
+
+        # Allow up to 5 seconds for the action server to come up
+        self.move_base.wait_for_server(rospy.Duration(5))
+
+    def goto(self, _pose):
+        # Send a goal
+        self.goal_sent = True
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose = _pose
+
+        # Start moving
+        self.move_base.send_goal(goal)
+
+        # Allow TurtleBot up to 60 seconds to complete task
+        success = self.move_base.wait_for_result(rospy.Duration(60)) 
+
+        state = self.move_base.get_state()
+        result = False
+
+        if success and state == GoalStatus.SUCCEEDED:
+            # We made it!
+            result = True
+        else:
+            self.move_base.cancel_goal()
+
+        self.goal_sent = False
+        return result
+
+    def shutdown(self):
+        if self.goal_sent:
+            self.move_base.cancel_goal()
+        rospy.loginfo("Stop")
+        rospy.sleep(1)
+
+temp_pose = null
+
+def get_cur_pose(data):
+    temp_pose = data.pose.pose
+
+ def handle(msg):
+        """
+        хендлер выполняется в отдельном потоке,
+        вызывается событием на подобие блокирующей очереди
+        """
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        print(content_type, chat_type, chat_id)
+        if chat_id == self.chat_id:
+            if content_type == 'text':
+                self.debug_message(self.new_command(msg['text']))
+            else:
+                self.debug_message("Ошибка 2! Неверный тип: только text и location")
+        else:
+            self.debug_message("Ошибка 1! Access error!")
+
+def main():
+    rospy.init_node('turtle_express')
+
+    TOKEN = load_param('~token')
+    CHAT_ID = load_param('~chat_id')
+    PROXY = load_param('~proxy')
+    DEBUG = load_param('~debug')
+    _tbot = telepot.Bot(TOKEN)
+    if PROXY != None: telepot.api.set_proxy(PROXY)
+    MessageLoop(_bot, handle).run_as_thread()
+
+
+    rospy.loginfo('Inited node turtle_express')
+    rospy.Subscriber('/amcl_pose', PoseWithCovarianceStamped, get_cur_pose)
+
+    st = StateMachine()
+
+    navigator = GoToPose()
+
+    # Customize the following values so they are appropriate for your location
+    # pos = {'x': 1.22, 'y' : 2.56}
+    # quat = {'r1' : 0.000, 'r2' : 0.000, 'r3' : 0.000, 'r4' : 1.000}
+    # my_pose = Pose(Point(pos['x'], pos['y'], 0.000), Quaternion(quat['r1'], quat['r2'], quat['r3'], quat['r4'])
+
+    my_pose = temp_pose
+
+    time.sleep(10)
+
+    success = navigator.goto(my_pose)
+
+    if success:
+        rospy.loginfo("Hooray, reached the desired pose")
+    else:
+        rospy.loginfo("The base failed to reach the desired pose")
+
+    # goto = rospy.Publisher('/move_base/goal', _MoveBaseActionGoal)
+    # goto.publish()
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
 
 
 
