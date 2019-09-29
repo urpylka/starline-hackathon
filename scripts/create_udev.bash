6476c40f486a27831f1b0d3f82e55d96c0e0b22c
Коваленок Александр <kovalenok.av@starline.ru>
2019-09-29 14:29:57 +0300

    Changed to local file rules

--- scripts/create_udev.bash
+++ scripts/create_udev.bash
@@ -2,11 +2,11 @@
 echo "[INFO] Setting up udev rules..."
 
 # RPLidar
-sudo cp /catkin_ws/src/rplidar_ros/scripts/rplidar.rules /etc/udev/rplidar_rules.d
+sudo cp scripts/rplidar.rules /etc/udev/rplidar_rules.d
 # Kobuki
-sudo cp kobuki.rules /etc/udev/kobuki_rules.d
+sudo cp scripts/kobuki.rules /etc/udev/kobuki_rules.d
 # Astra
-sudo cp /catkin_ws/src/ros_astra_camera/56-orbbec-usb.rules /etc/udev/astra_rules.d
+sudo cp scripts/astra.rules /etc/udev/astra_rules.d
 
 echo "[INFO] Restarting udev service..."
 sudo service udev reload
