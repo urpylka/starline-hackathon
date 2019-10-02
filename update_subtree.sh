c4c1ca6f87893bf2fb6eb222d91a037e58055649
Nikolay Dema <dema.nu@starline.ru>
2019-10-02 15:48:03 +0300

    miss in tws folder name

--- update_subtree.sh
+++ update_subtree.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git subtree pull --prefix driver_tws/src/rplidar_ros/       https://github.com/robopeak/rplidar_ros.git     master   --squash -m "auto-update subtree" && \
-git subtree pull --prefix driver_tws/src/ros_astra_camera/  https://github.com/orbbec/ros_astra_camera.git  master   --squash -m "auto-update subtree" && \
-git subtree pull --prefix driver_tws/src/kobuki/            https://github.com/yujinrobot/kobuki.git        kinetic  --squash -m "auto-update subtree" && \
-git subtree pull --prefix driver_tws/src/kobuki_msg/        https://github.com/yujinrobot/kobuki_msgs.git   kinetic  --squash -m "auto-update subtree" && \
-git subtree pull --prefix driver_tws/src/kobuki_core/       https://github.com/yujinrobot/kobuki_core.git   kinetic  --squash -m "auto-update subtree"
+git subtree pull --prefix drivers_tws/src/rplidar_ros/       https://github.com/robopeak/rplidar_ros.git     master   --squash -m "auto-update subtree" && \
+git subtree pull --prefix drivers_tws/src/ros_astra_camera/  https://github.com/orbbec/ros_astra_camera.git  master   --squash -m "auto-update subtree" && \
+git subtree pull --prefix drivers_tws/src/kobuki/            https://github.com/yujinrobot/kobuki.git        kinetic  --squash -m "auto-update subtree" && \
+git subtree pull --prefix drivers_tws/src/kobuki_msg/        https://github.com/yujinrobot/kobuki_msgs.git   kinetic  --squash -m "auto-update subtree" && \
+git subtree pull --prefix drivers_tws/src/kobuki_core/       https://github.com/yujinrobot/kobuki_core.git   kinetic  --squash -m "auto-update subtree"
