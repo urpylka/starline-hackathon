d9660f2ca1cfe6942a01363e2e2755eefd0ee825
Nikolay Dema <dema.nu@starline.ru>
2019-10-20 14:51:20 +0300

    wrong rules names

--- scripts/create_udev.bash
+++ scripts/create_udev.bash
@@ -2,11 +2,11 @@
 echo "[INFO] Setting up udev rules..."
 
 # RPLidar
-sudo cp rplidar.rules /etc/udev/rules.d/rplidar_rules.d
+sudo cp rplidar.rules /etc/udev/rules.d
 # Kobuki
-sudo cp kobuki.rules /etc/udev/rules.d/kobuki_rules.d
+sudo cp kobuki.rules /etc/udev/rules.d
 # Astra
-sudo cp astra.rules /etc/udev/rules.d/astra_rules.d
+sudo cp astra.rules /etc/udev/rules.d
 
 echo "[INFO] Restarting udev service..."
 sudo service udev reload
