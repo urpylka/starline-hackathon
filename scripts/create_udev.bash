b7eec01a9cd86084ba9a4bbefacae88751d8b9fa
Караваев Александр <karavaev.aa@starline.ru>
2019-10-20 12:53:18 +0300

    Small typo fixes

--- scripts/create_udev.bash
+++ scripts/create_udev.bash
@@ -2,11 +2,11 @@
 echo "[INFO] Setting up udev rules..."
 
 # RPLidar
-sudo cp scripts/rplidar.rules /etc/udev/rules.d/rplidar_rules.d
+sudo cp rplidar.rules /etc/udev/rules.d/rplidar_rules.d
 # Kobuki
-sudo cp scripts/kobuki.rules /etc/udev/rules.d/kobuki_rules.d
+sudo cp kobuki.rules /etc/udev/rules.d/kobuki_rules.d
 # Astra
-sudo cp scripts/astra.rules /etc/udev/rules.d/astra_rules.d
+sudo cp astra.rules /etc/udev/rules.d/astra_rules.d
 
 echo "[INFO] Restarting udev service..."
 sudo service udev reload
--- scripts/setup.bash
+++ scripts/setup.bash
@@ -4,12 +4,12 @@
 # Installing some useful stuff
 #
 
-sudo apt-get install vim mc ssh
+sudo apt-get install -y vim mc ssh
 
 # Install docker
 # sudo apt-get remove docker docker-engine docker.io containerd runc
-sudo apt-get update
-sudo apt-get install \
+sudo apt-get update -y
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -21,8 +21,8 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
