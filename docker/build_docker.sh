2bb73c8f7c2d72f929403cb00d9560798949979c
Nikolay Dema <dema.nu@starline.ru>
2020-02-13 22:28:45 +0300

    add kobuki in gazebo

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
 
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+EXEC_PATH=$PWD
 
-docker build -t kobuki-sl-hackathon-sim -f $ROOT_DIR/docker/Dockerfile $ROOT_DIR --network=host
+cd $ROOT_DIR
+
+if [[ $1 = "--nvidia" ]] || [[ $1 = "-n" ]]
+  then
+    docker build -t kobuki-sl-hackathon-sim -f $ROOT_DIR/docker/Dockerfile $ROOT_DIR \
+                                           --network=host \
+                                           --build-arg from=nvidia/opengl:1.1-glvnd-runtime-ubuntu16.04
+
+else
+    echo "[!] If you wanna use nvidia gpu, please rebuild with -n or --nvidia argument"
+    docker build -t kobuki-sl-hackathon-sim -f $ROOT_DIR/docker/Dockerfile $ROOT_DIR \
+                                           --network=host \
+                                           --build-arg from=ubuntu:18.04
+fi
+
+cd $EXEC_PATH
---
+++ scripts/host_setup.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Install docker
+sudo apt-get update -y
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+
+# Install docker-compose
+
+#sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#sudo chmod +x /usr/local/bin/docker-compose
+#docker-compose --version
+
+
+# NVIDIA Container Toolkit
+if [[ $1 = "--nvidia" ]] || [[ $1 = "-n" ]]
+  then
+      distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+      curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
+      curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+
+      sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+      sudo systemctl restart docker
+fi
