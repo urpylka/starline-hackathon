e74cca6477df3787a37c504352f1f6b541411589
Artem Smirnov <urpylka@gmail.com>
2020-03-18 13:56:29 +0400

    Moved update_subtree script to scripts folder

--- update_subtree.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git subtree pull --prefix drivers_ws/src/rplidar_ros/       https://github.com/robopeak/rplidar_ros.git     master   --squash -m "auto-update subtree" && \
-git subtree pull --prefix drivers_ws/src/ros_astra_camera/  https://github.com/orbbec/ros_astra_camera.git  master   --squash -m "auto-update subtree" && \
-git subtree pull --prefix drivers_ws/src/kobuki/            https://github.com/yujinrobot/kobuki.git        kinetic  --squash -m "auto-update subtree" && \
-git subtree pull --prefix drivers_ws/src/kobuki_msg/        https://github.com/yujinrobot/kobuki_msgs.git   kinetic  --squash -m "auto-update subtree" && \
-git subtree pull --prefix drivers_ws/src/kobuki_core/       https://github.com/yujinrobot/kobuki_core.git   kinetic  --squash -m "auto-update subtree"
