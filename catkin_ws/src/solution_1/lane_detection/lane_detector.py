7f9d9452065860970ae44fcd59610e69664834f8
Arthur Golubtsov <goldartt@gmail.com>
2020-03-23 01:26:08 +0300

    Add image undistort to lane detector

--- catkin_ws/src/solution_1/lane_detection/lane_detector.py
+++ catkin_ws/src/solution_1/lane_detection/lane_detector.py
@@ -5,12 +5,14 @@ from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from dynamic_reconfigure.server import Server
 from solution_1.cfg import lane_detector_Config
+from camera_undistort import get_matrix_and_distortions_ros, get_undistorted_image
 
-class ThresholdParams:
+class DynParams:
 
-    params_default_dict = {
-        "function": 1,
-        "max_val": 255,
+    default_dict = {
+        "undistort_image": True,
+        "thr_function": 1,
+        "thr_max_val": 255,
         "thr_val": 127,
         "thr_type": cv2.THRESH_BINARY,
         "thr_type_flag": 0,
@@ -21,35 +23,56 @@ class ThresholdParams:
     }
 
     def __init__(self):
-        for key, value in self.params_default_dict.items():
+        for key, value in self.default_dict.items():
             setattr(self, key, value)
 
 cv_bridge = CvBridge()
-thr_params = ThresholdParams()
+dyn_params = DynParams()
 last_image = None
+matrix = None
+distortions = None
 
 def image_callback(msg):
     global last_image
     last_image = msg
 
-def dyn_param_callback(dyn_params, level):
-    for key, value in dyn_params.items():
-        setattr(thr_params, key, value)
-    print(thr_params.thr_val)
-    return dyn_params
+def dyn_param_callback(config, level):
+    for key, value in config.items():
+        setattr(dyn_params, key, value)
+    return config
 
-def threshold(cv2_image, thr_params):
+def threshold(cv2_image, params):
     try:
-        if thr_params.function == 0:
-            _, thr_image = cv2.threshold(cv2_image, thr_params.thr_val, thr_params.max_val, thr_params.thr_type+thr_params.thr_type_flag)
-        elif thr_params.function == 1:
-            thr_image = cv2.adaptiveThreshold(cv2_image, thr_params.max_val, thr_params.a_thr_method, thr_params.a_thr_type, thr_params.a_thr_blocksize, thr_params.a_thr_C)
+        if params.thr_function == 0:
+            _, thr_image = cv2.threshold(cv2_image, params.thr_val, params.thr_max_val, params.thr_type+params.thr_type_flag)
+        elif params.thr_function == 1:
+            thr_image = cv2.adaptiveThreshold(cv2_image, params.thr_max_val, params.a_thr_method, params.a_thr_type, params.a_thr_blocksize, params.a_thr_C)
     except cv2.error as e:
         rospy.logerr("Can't threshold image. Error: {}".format(e))
         return cv2_image
     return thr_image
 
+def prepare_image(cv2_image, params, matrix=None, distortions=None):
+    undistorted_image = cv2_image
+    if params.undistort_image:
+        if matrix is not None and distortions is not None:
+            undistorted_image = get_undistorted_image(cv2_image, matrix, distortions)
+    gray = cv2.cvtColor(undistorted_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    thr = threshold(blur, params)
+    return thr
+
+def pub_image(cv2_image, publisher, header):
+    if publisher.get_num_connections() > 0:
+        m = cv_bridge.cv2_to_imgmsg(cv2_image)
+        m.header.stamp.secs = header.stamp.secs
+        m.header.stamp.nsecs = header.stamp.nsecs
+        publisher.publish(m)
+
+
 if __name__ == "__main__":
+
+    global matrix, distortions
     rospy.init_node('lane_detector')
     srv = Server(lane_detector_Config, dyn_param_callback)
 
@@ -58,6 +81,11 @@ if __name__ == "__main__":
 
     image_sub = rospy.Subscriber('image_raw', Image, image_callback)
     image_pub = rospy.Publisher('threshold', Image, queue_size=1)
+
+    matrix, distortions = get_matrix_and_distortions_ros('camera_info')
+
+    print matrix
+    print distortions
 
     rate = rospy.Rate(_rate)
 
@@ -71,16 +99,9 @@ if __name__ == "__main__":
 
         last_image_cv2 = cv_bridge.imgmsg_to_cv2(last_image)
 
-        gray = cv2.cvtColor(last_image_cv2, cv2.COLOR_RGB2GRAY)
-        blur = cv2.GaussianBlur(gray,(5,5),0)
+        prepared_image = prepare_image(last_image_cv2, dyn_params, matrix, distortions)
 
-        th = threshold(blur, thr_params)
-
-        if image_pub.get_num_connections() > 0:
-            m = cv_bridge.cv2_to_imgmsg(th)
-            m.header.stamp.secs = header.stamp.secs
-            m.header.stamp.nsecs = header.stamp.nsecs
-            image_pub.publish(m)
+        pub_image(prepared_image, image_pub, header)
 
     '''
     matrix, distortions = get_matrix_and_distortions_ros(args.namespace+'/camera_info')
