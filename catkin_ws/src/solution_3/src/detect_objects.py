a96b532229b9bcd1dcfa0a3c97608da02c76dedd
Artem Smirnov <urpylka@gmail.com>
2020-03-26 12:03:17 +0300

    Update solution_3

--- catkin_ws/src/solution_3/src/detect_objects.py
+++ catkin_ws/src/solution_3/src/detect_objects.py
@@ -7,6 +7,13 @@
 # Author:       Artem Smirnov @urpylka, Artur Golubtsov @goldarte
 # Description:  Detect objects
 
+from enum import Enum
+class SemaphoreState(Enum):
+    RED = 1
+    GREEN = 2
+    YELLOW = 3
+
+
 class DetectObjects(object):
 
     def __init__(self):
@@ -20,3 +27,7 @@ class DetectObjects(object):
 
     def detectedSemaphore(self):
         return True # or False
+
+
+    def getStateSemaphore(self):
+        return SemaphoreState.RED
