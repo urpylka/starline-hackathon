6d632d649b9227abb9aecc980d707ce531bfa39b
Artem Smirnov <urpylka@gmail.com>
2020-03-27 16:01:16 +0300

    FIX

--- catkin_ws/src/solution_3/src/detect_objects.py
+++ catkin_ws/src/solution_3/src/detect_objects.py
@@ -22,12 +22,12 @@ detected_redlight = rospy.ServiceProxy('/detected_redlight', Trigger)
 class DetectObjects(object):
 
     def detectedStopSign(self):
-        return False
-        # return detected_stop().success
+        # return False
+        return detected_stop().success
 
     def detectedSemaphoreRed(self):
-        return False
-        # return detected_redlight().success
+        # return False
+        return detected_redlight().success
 
 if __name__ == "__main__":
     d = DetectObjects()
